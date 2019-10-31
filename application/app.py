b3aef49d8dfaac64b10bef3c62e742716ebedca5
root <root@instance-1.us-central1-a.c.team08-project.internal>
2019-10-31 21:31:08 +0000

    Added dropdown/search functionality with database info

--- application/app.py
+++ application/app.py
@@ -1,39 +1,93 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, request, redirect
+from flaskext.mysql import MySQL
 
-from flask import Flask, render_template
 app = Flask(__name__)
+
+# Connect to the MySQL database
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'prototypedb'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql = MySQL()
+mysql.init_app(app)
 
 @app.route('/ourTeam')
 def outTeam():
     return render_template('about.html')
 
+
 @app.route('/about_Alex')
 def alexProfile():
     return render_template('about_team_members/about_Alex.html')
+
 
 @app.route('/about_AlexLee')
 def alexLeeProfile():
     return render_template('about_team_members/about_AlexLee.html')
 
+
 @app.route('/about_Andrew')
 def andrewProfile():
     return render_template('about_team_members/about_Andrew.html')
+
 
 @app.route('/about_Emanuel')
 def emanuelProfile():
     return render_template('about_team_members/about_Emanuel.html')
 
+
 @app.route('/about_Kevin')
 def kevinProfile():
     return render_template('about_team_members/about_Kevin.html')
+
 
 @app.route('/about_Tim')
 def timProfile():
     return render_template('about_team_members/about_Tim.html')
 
-@app.route('/')
-@app.route('/index')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT name from categories ORDER BY name ASC")
+    conn.commit()
+    cats = cursor.fetchall()
+    if request.method == "POST":
+        category = request.form['category']
+        item = request.form['item']
+        # all in the search box will return all the tuples
+        if len(item) == 0:
+            if category == "All":
+                cursor.execute("SELECT P.title, P.description, C.name FROM posts P, categories C WHERE P.category=C.cID")
+                conn.commit()
+                data = cursor.fetchall()
+            else:
+                cursor.execute("SELECT P.title, P.description, C.name FROM posts P, categories C WHERE P.category = C.cID AND C.name = '%s'" %(category))
+                conn.commit()
+                data = cursor.fetchall()
+        # search by category
+        else:
+            if category == "All":
+                cursor.execute("SELECT P.title, P.description, C.name FROM posts P JOIN categories C ON P.category = C.cID WHERE P.description LIKE '%%%s%%' OR P.title LIKE '%%%s%%'" %(item, item))
+                conn.commit()
+                data = cursor.fetchall()
+            else:
+                cursor.execute("SELECT P.title, P.description, C.name FROM posts P JOIN categories C ON P.category = C.cID WHERE C.name = '%s' AND (P.description LIKE '%%%s%%' OR P.title LIKE '%%%s%%')" %(category, item, item))
+                conn.commit()
+                data = cursor.fetchall()
+            conn.close()
+        return render_template('search.html', data=data, cats=cats)
+    conn.close()
+    return render_template('search.html', cats=cats)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
+
